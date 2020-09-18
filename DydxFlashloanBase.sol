fc2e29ec802faeea7031378b59795d60c9bf7c62
spikeruk <30730481+spikeruk@users.noreply.github.com>
2020-09-18 15:20:48 +0100

    Update DydxFlashloanBase.sol

--- DydxFlashloanBase.sol
+++ DydxFlashloanBase.sol
@@ -2,8 +2,6 @@
 pragma solidity >=0.6.0 <0.7.0;
 pragma experimental ABIEncoderV2;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "./IDydx.sol";
 
