04cf47f1887a5a6dfdf7f0c21a5884ec07a61ae9
spikeruk <30730481+spikeruk@users.noreply.github.com>
2020-09-18 15:34:39 +0100

    Update DydxFlashloanBase.sol

--- DydxFlashloanBase.sol
+++ DydxFlashloanBase.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier:
-pragma solidity >=0.6.0 <0.7.0;
+pragma solidity >=0.5.0 <0.7.0;
 pragma experimental ABIEncoderV2;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
