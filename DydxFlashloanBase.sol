253aa24d87dec9b730719c15208ec919f26bdfe8
spikeruk <30730481+spikeruk@users.noreply.github.com>
2020-09-18 15:26:40 +0100

    Update DydxFlashloanBase.sol

--- DydxFlashloanBase.sol
+++ DydxFlashloanBase.sol
@@ -4,7 +4,7 @@ pragma experimental ABIEncoderV2;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/IERC20.sol";
-import "./IDydx.sol";
+import "https://github.com/spikeruk/SmartContract/blob/master/IDydx.sol";
 
 contract DydxFlashloanBase {
     using SafeMath for uint256;
